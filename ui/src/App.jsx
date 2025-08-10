55062d7fbd7190ff132de72cbaf6fa490a27a2ba
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-10 21:25:50 +0100

    combine to App

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,11 +1,18 @@
 
 import './App.css'
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Menu from "./components/Menu.jsx";
 
 function App() {
 
   return (
     <>
-     <h1>Hello</h1>
+    <div className="container">
+        <Header />
+        <Menu/>
+        <Footer/>
+    </div>
     </>
   )
 }
